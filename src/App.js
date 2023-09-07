27de53d1225fabc75d577391c3dc3d54796d8481
cleusia <cleusiaanjos@hotmail.com>
2023-09-07 02:31:09 +0100

    Header and beginning of film info component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Header from './Components/Header'
+import Header from './Components/Header';
+import FilmInfo from './Components/FilmInfo';
+import './App.css'
 
 function App() {
   return (
     <div >
 
       <Header></Header>
-      
+      <FilmInfo></FilmInfo>
+
         
     </div>
   );
